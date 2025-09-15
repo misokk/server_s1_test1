93f29e0e068a75c393b69bc791603d4b354a97ad
Kang, Miso <miso888836@gmail.com>
2025-09-15 23:38:53 +0900

    five

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("강미소");
         World.run();
+        World.run2();
     }
 }
--- World.java
+++ World.java
@@ -5,4 +5,8 @@ public class World {
         System.out.println("202415014");
     }
     
+    public static void run2() {
+        System.out.println("hello world");
+    }
+    
 }

ac22a60ca469df57ac9881a305ab8314522656df
Kang, Miso <miso888836@gmail.com>
2025-09-15 23:35:25 +0900

    four

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+package studentA.server_s1_test1;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("강미소");
+        System.out.println("강미소bb");
         World.run();
+        Hello.run();
     }
 }
